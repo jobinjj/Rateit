8f166748db5ea900d3715a7590be95853c6662ec
jobinjj <jobinj1048@gmail.com>
2018-03-12 03:23:58 -0700

    third commit

--- app/src/main/java/com/techpakka/rateit/LoginActivity.java
+++ app/src/main/java/com/techpakka/rateit/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.techpakka.rateit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void gotoRegister(View view) {
+        Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/techpakka/rateit/RegisterActivity.java
+++ app/src/main/java/com/techpakka/rateit/RegisterActivity.java
@@ -1,18 +1,26 @@
 package com.techpakka.rateit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText ed1,ed2,ed3;
+    String email,pass,cpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initViews();
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/Montserrat-Regular.ttf")
@@ -20,8 +28,31 @@ public class RegisterActivity extends AppCompatActivity {
                 .build()
         );
     }
+
+    private void initViews() {
+        ed1 = findViewById(R.id.ed1);
+        ed2 = findViewById(R.id.editText);
+        ed3 = findViewById(R.id.editText2);
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = ed1.getText().toString();
+                pass = ed2.getText().toString();
+                cpass = ed3.getText().toString();
+                Toast.makeText(RegisterActivity.this, email + " " + pass + " " + cpass, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+
+    public void gotoLogin(View view) {
+        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(intent);
+
     }
 }
